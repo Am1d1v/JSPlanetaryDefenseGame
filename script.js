ea2a0a8fdee1b4712eed302f128a6821b9581331
Am1d1v <dimadiman1adima@gmail.com>
2024-01-29 17:37:35 +0300

    Planet Class and context configuration

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 
+// Planer Class
+class Planet{
+    constructor() {
+        this.x = 200;
+        this.y = 200;
+        this.radius = 80;
+    }
+
+    draw(context){
+        context.beginPath();
+        context.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+        context.stroke();
+    }
+}
 
 window.addEventListener('load', () => {
 
@@ -9,4 +23,12 @@ window.addEventListener('load', () => {
     // Canvas Width and Height
     canvas.width = 800;
     canvas.height = 800;
+
+    // Context Configuration
+    context.strokeStyle = 'white';
+    context.lineWidth = 2;
+
+    // Instantiate Planet Class
+    const planet = new Planet();
+    planet.draw(context)
 });
