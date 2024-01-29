661325b743e2a82ec0a16e92ea379f1e030c8ba0
Am1d1v <dimadiman1adima@gmail.com>
2024-01-29 15:26:47 +0300

    Basic Setup

---
+++ script.js
@@ -0,0 +1,12 @@
+
+
+window.addEventListener('load', () => {
+
+    // Canvas and Canvas Context
+    const canvas = document.querySelector('#canvas1');
+    const context = canvas.getContext('2d');
+
+    // Canvas Width and Height
+    canvas.width = 800;
+    canvas.height = 800;
+});
