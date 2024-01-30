2d89c044ff1254bd823ba861de50b26e6e31fe5a
Am1d1v <dimadiman1adima@gmail.com>
2024-01-30 13:31:27 +0300

    Set Player's Rotation Angle

--- script.js
+++ script.js
@@ -26,21 +26,29 @@ class Player{
         this.radius = 40;
         this.image = document.querySelector('#player');
         this.aim;
+        this.angle = 0;
     }
 
     // Draw Player and Set Player's initial coordinated
     draw(context){
-        context.drawImage(this.image, this.x - this.radius, this.y - this.radius)
+        context.save();
+        context.translate(this.x, this.y)
+        context.rotate(this.angle);
+        context.drawImage(this.image, -this.radius, -this.radius)
         context.beginPath();
-        context.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+        context.arc(0, 0, this.radius, 0, Math.PI * 2);
         context.stroke();
+        context.restore();
     }
 
     // Update Player's position
     update(){
-        this.aim = this.game.calcAim(this.game.mouse, this.game.planet)
+        this.aim = this.game.calcAim(this.game.planet, this.game.mouse)
         this.x = this.game.planet.x + this.game.planet.radius * this.aim[0];
         this.y = this.game.planet.y + this.game.planet.radius * this.aim[1];
+
+        // Player's Rotation
+        this.angle = Math.atan2(this.aim[3], this.aim[2]);
     }
 }
 
@@ -72,9 +80,6 @@ class Game {
         this.player.draw(context);
         this.player.update()
         context.beginPath();
-        context.moveTo(this.planet.x, this.planet.y);
-        context.lineTo(this.mouse.x, this.mouse.y);
-        context.stroke();
     }
 
     // Set Player's cooidinates depending on where the currently mouse is 
@@ -87,8 +92,8 @@ class Game {
         const distance = Math.hypot(dx, dy);
 
         // Player's Horizontal and Vertical Directions
-        const aimX = dx / distance;
-        const aimY = dy / distance;
+        const aimX = dx / distance * -1;
+        const aimY = dy / distance * -1;
 
         return [aimX, aimY, dx, dy]
     }
